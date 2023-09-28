703bb24134856b42cf5a96467fda89a794d625f5
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-09-28 18:16:49 +0530

    aboutPage-completed

---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./aboutMe.css";
+import Button from "react-bootstrap/esm/Button";
+import { FaFileArrowDown } from "react-icons/fa6";
+import image from "../../images/photo.jpeg"
+
+function AboutMe() {
+  return (
+    <div>
+      <section className="aboutMe-section">
+        <div className="text">
+          <h3>Who I Am ----------------------------------</h3>
+          <p>
+            Hello, my name is Saravanan. I completed BBA degree in 2020 and subsequently
+            gained work experience in the garments sector for a few years.
+            However, I decided to pivot my career and pursue my newfound
+            interest in web development. I enrolled in a web development course
+            at Skill Safari, where I honed my skills in various aspects of web
+            development, with a focus on React.
+            <br/><br/>
+            During the course, I had the
+            opportunity to work on numerous projects, allowing me to apply and
+            solidify my knowledge. Now, I am actively seeking job opportunities
+            to further my career growth and expand my expertise in the field of
+            web development.
+          </p>
+          <Button variant="outline-dark"><FaFileArrowDown/> Resume</Button>
+        </div>
+        <div className="image">
+          <img src={image} alt="profilePhoto"/>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AboutMe;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import {
@@ -10,38 +11,116 @@ import {
   FaSquarePhone,
 } from "react-icons/fa6";
 import "./header.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
+import Headroom from "react-headroom";
+import {useNavigate} from "react-router-dom"
 
 function Header() {
+  const navigate =  useNavigate()
+
+  const handleNavigation = () => {
+    navigate("/")
+  }
   return (
     <div>
-      <Navbar expand="lg" style={{ background: "#000" }} className="shadow">
-        <h6 style={{ color: "rgb(100, 255, 218)" }}>S</h6>
-        <div className="container-fluid justify-content-between">
-          <Link to={"/"}>
-            <Navbar.Brand style={{ color: "rgb(204, 214, 246)", fontSize: "20px" }}>
-              <FaHornbill className="logo" />
+      <Headroom style={{ zIndex: "100" }}>
+        <Navbar expand="lg" style={{ background: "rgba(0, 0, 0, 0.7)" }} className="shadow">
+          <h6 style={{ color: "rgb(100, 255, 218)" }}>S</h6>
+          <div className="container-fluid justify-content-between">
+            <Navbar.Brand
+              style={{ color: "rgb(204, 214, 246)", fontSize: "20px" }}
+            >
+              <FaHornbill className="logo"  onClick={handleNavigation} style={{cursor : "pointer"}}/>
             </Navbar.Brand>
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className="home me-4" style={{ color: "rgb(204, 214, 246)"}}>
-                <FaHouseChimney className="pb-1" /> START HERE
-              </Nav.Link>
-              <Nav.Link href="#about" className="home me-4" style={{ color: "rgb(204, 214, 246)"}}>
-                <FaBookOpenReader className="pb-1" /> WHO I AM?
-              </Nav.Link>
-              <Nav.Link href="#projects" className="home me-4" style={{ color: "rgb(204, 214, 246)"}}>
-                <FaRectangleList className="pb-1" /> CREATIONS
-              </Nav.Link>
-              <Nav.Link href="#contact" className="home" style={{ color: "rgb(204, 214, 246)"}}>
-                <FaSquarePhone className="pb-1" /> GET IN TOUCH
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-      </Navbar>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Link
+                  activeClass="active"
+                  to="home"
+                  className="home  me-4 mt-1"
+                  style={{ color: "rgb(204, 214, 246)" }}
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={100}
+                 
+                >
+                  {" "}
+                  <FaHouseChimney className="pb-1" /> START HERE
+                </Link>
+                <Link
+                  activeClass="active"
+                  to="about"
+                  className="home me-4 mt-1"
+                  style={{ color: "rgb(204, 214, 246)" }}
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={100}
+                >
+                  {" "}
+                  <FaBookOpenReader className="pb-1" /> WHO I AM?
+                </Link>
+                <Link
+                  activeClass="active"
+                  to="qualifications"
+                  className="home me-4 mt-1"
+                  style={{ color: "rgb(204, 214, 246)" }}
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={100}
+                >
+                  {" "}
+                  <FaBookOpenReader className="pb-1" />
+                  QUALIFICATIONS
+                </Link>
+                <Link
+                  activeClass="active"
+                  to="Abilities"
+                  className="home me-4 mt-1"
+                  style={{ color: "rgb(204, 214, 246)" }}
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={100}
+                >
+                  {" "}
+                  <FaBookOpenReader className="pb-1" />
+                  ABILITIES
+                </Link>
+                <Link
+                  activeClass="active"
+                  to="projects"
+                  className="home me-4 mt-1"
+                  style={{ color: "rgb(204, 214, 246)" }}
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={100}
+                >
+                  {" "}
+                  <FaRectangleList className="pb-1" /> CREATIONS
+                </Link>
+                <Link
+                  activeClass="active"
+                  to="contact"
+                  className="home me-4 mt-1"
+                  style={{ color: "rgb(204, 214, 246)" }}
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={100}
+                >
+                  {" "}
+                  <FaSquarePhone className="pb-1" /> GET IN TOUCH
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Navbar>
+      </Headroom>
     </div>
   );
 }
