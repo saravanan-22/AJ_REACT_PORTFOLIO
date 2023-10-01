34db77b0de3d45b6b1b71fef79bf43a01f71f8c4
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-10-01 11:51:22 +0530

    projectPage-completed

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./aboutMe.css";
 import Button from "react-bootstrap/esm/Button";
 import { FaFileArrowDown } from "react-icons/fa6";
-import image from "../../images/photo.png"
+import image from "../../images/photo.png";
 
 function AboutMe() {
   return (
@@ -11,24 +11,26 @@ function AboutMe() {
         <div className="text">
           <h3>Who I Am ----------------------------------</h3>
           <p>
-            Hello, my name is Saravanan. I completed BBA degree in 2020 and subsequently
-            gained work experience in the garments sector for a few years.
-            However, I decided to pivot my career and pursue my newfound
-            interest in web development. I enrolled in a web development course
-            at Skill Safari, where I honed my skills in various aspects of web
-            development, with a focus on React.
-            <br/><br/>
-            During the course, I had the
-            opportunity to work on numerous projects, allowing me to apply and
-            solidify my knowledge. Now, I am actively seeking job opportunities
-            to further my career growth and expand my expertise in the field of
-            web development.
+            "👋 Hello, my name is Saravanan. 🎓 I completed my BBA degree in
+            2020 and subsequently gained work experience in the garments sector
+            for a few years. However, I decided to pivot my career and pursue my
+            newfound interest in web development. 🌐 I enrolled in a web
+            development course at Skill Safari, where I honed my skills in
+            various aspects of web development, with a focus on React.
+            <br />
+            <br />
+            🛠️ During the course, I had the opportunity to work on numerous
+            projects, allowing me to apply and solidify my knowledge. 🚀 Now, I
+            am actively seeking job opportunities to further my career growth
+            and expand my expertise in the field of web development."
           </p>
-          <Button variant="warning"><FaFileArrowDown/> Resume</Button>
+          <Button variant="warning">
+            <FaFileArrowDown /> Resume
+          </Button>
         </div>
         <div className="image">
           <div></div>
-          <img src={image} alt="profilePhoto" className="photo"/>
+          <img src={image} alt="profilePhoto" className="photo" />
         </div>
       </section>
     </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./projects.css";
 import Carousel from "react-bootstrap/Carousel";
 import project1 from "../../images/quiz_image1.jpg";
@@ -22,6 +22,16 @@ import project2Image10 from "../../images/oqdb_image10.jpg";
 import project2Image11 from "../../images/oqdb_image11.jpg";
 import project2Image12 from "../../images/oqdb_image12.jpg";
 import project2Image13 from "../../images/oqdb_image13.jpg";
+import project3Image1 from "../../images/movie_app_image1.jpg";
+import project3Image2 from "../../images/movie_app_image2.jpg";
+import project3Image3 from "../../images/movie_app_image3.jpg";
+import project3Image4 from "../../images/movie_app_image4.jpg";
+import project3Image5 from "../../images/movie_app_image5.jpg";
+import project3Image6 from "../../images/movie_app_image6.jpg";
+import project3Image7 from "../../images/movie_app_image7.jpg";
+import rwi1 from "../../images/rwi1.jpg";
+import rwi2 from "../../images/rwi2.jpg";
+import rwi3 from "../../images/rwi3.jpg";
 import html from "../../images/htmlLogo.png";
 import css from "../../images/cssLogo.png";
 import bootstrap from "../../images/bootstrapLogo.png";
@@ -32,11 +42,13 @@ import express from "../../images/expressLogo.png";
 import mongodb from "../../images/mongodbLogo.png";
 import github from "../../images/github.jpg";
 import material from "../../images/material Ui.png";
-import vercel from "../../images/react.jpg";
+import vercel from "../../images/vercel.jpg";
 import cyclic from "../../images/cyclic.jpg";
 import firebase from "../../images/firebaseLogo.png";
-import { Button } from "react-bootstrap";
+import movieapi from "../../images/movieDb.jpg";
+import { Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import gifImage from "../../images/project gif.gif";
 
 const Projects = () => {
   return (
@@ -159,6 +171,7 @@ const Projects = () => {
             our MERN-based quiz game website offers an enjoyable and educational
             experience for all.
           </p>
+          <h5 style={{ color: "rgb(156, 164, 188)" }}>Technology Stack : </h5>
           <div className="mernImages">
             <img src={html} />
             <img src={css} />
@@ -176,7 +189,7 @@ const Projects = () => {
           </div>
         </div>
       </section>
-      <hr style={{ color: "rgb(88, 224, 191)", margin: "3em 0" }} />
+      <hr className="animated-hr" />
       <section>
         <div>
           <h5
@@ -305,7 +318,8 @@ const Projects = () => {
         >
           <section>
             <h4 style={{ color: "rgb(193, 203, 233)" }}>
-              OPEN QUESTION DATABASE WEBSITE
+              <span style={{ color: "rgb(88, 224, 191)" }}>01</span>.OPEN
+              QUESTION DATABASE WEBSITE
             </h4>
           </section>
           <section>
@@ -351,6 +365,7 @@ const Projects = () => {
             curiosity, learning, and discovery, as we build a global community
             of question enthusiasts and knowledge seekers.
           </p>
+          <h5 style={{ color: "rgb(156, 164, 188)" }}>Technology Stack : </h5>
           <div className="mernImages">
             <img src={html} />
             <img src={css} />
@@ -365,6 +380,206 @@ const Projects = () => {
             {/* <img src={cyclic} /> */}
             <img src={material} />
             <img src={firebase} />
+          </div>
+        </div>
+      </section>
+      <hr className="animated-hr" />
+      <section>
+        <div className="project2">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                src={project3Image1}
+                text="One slide"
+                className="firstSlide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={project3Image2}
+                text="Two slide"
+                className="firstSlide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={project3Image3}
+                text="Three slide"
+                className="firstSlide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={project3Image4}
+                text="Four slide"
+                className="firstSlide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={project3Image5}
+                text="Five slide"
+                className="firstSlide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={project3Image6}
+                text="Six slide"
+                className="firstSlide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={project3Image7}
+                text="Seven slide"
+                className="firstSlide"
+              />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "2em",
+          }}
+        >
+          <section>
+            <h4 style={{ color: "rgb(193, 203, 233)" }}>
+              <span style={{ color: "rgb(88, 224, 191)" }}>02</span>.MOVIE
+              WEBSITE
+            </h4>
+          </section>
+          <section>
+            <Link
+              to={"https://aj-react-p2-movieapp.vercel.app/"}
+              target="_blank"
+            >
+              <Button variant="outline-warning" className="me-2">
+                DEMO
+              </Button>
+            </Link>
+            <Link
+              to={"https://github.com/saravanan-22/AJ_REACT_P2_MOVIEAPP"}
+              target="_blank"
+            >
+              <Button variant="outline-warning">CODE</Button>
+            </Link>
+          </section>
+        </div>
+        <div>
+          <p
+            className="mt-2"
+            style={{
+              color: "rgb(136, 146, 176)",
+              textAlign: "justify",
+              fontSize: "1.2em",
+            }}
+          >
+            "🎬 Hello! This is my movie website project created using React. 🎥
+            Users can securely create their accounts. 🌟 If you have an interest
+            in movies, you can explore a list of all movies, including top-rated
+            films, currently showing movies, and trending movies. 🕵️‍♂️ You can
+            also search for specific movies to find exactly what you're looking
+            for. 🎯 Additionally, we've implemented categories to help you
+            choose movies tailored to different situations and preferences. 🎭
+            Whether you're in the mood for action, romance, comedy, or any other
+            genre, our website makes it easy to discover the perfect film for
+            any occasion. 🍿"
+          </p>
+          <h5 style={{ color: "rgb(156, 164, 188)" }}>Technology Stack : </h5>
+          <div className="mernImages">
+            <img src={html} />
+            <img src={css} />
+            <img src={bootstrap} />
+            <img src={js} />
+            <img src={react} />
+            {/* <img src={node} />
+            <img src={express} />
+            <img src={mongodb} /> */}
+            <img src={github} />
+            <img src={vercel} />
+            {/* <img src={cyclic} /> */}
+            <img src={material} />
+            <img src={firebase} />
+            <img src={movieapi} />
+          </div>
+        </div>
+      </section>
+      <hr className="animated-hr" />
+      <section>
+        <div>
+          <h5
+            style={{
+              color: "rgb(136, 146, 176)",
+              fontSize: "1.5em",
+            }}
+          >
+            JavaScript Projects{" "}
+          </h5>
+        </div>
+        <div className="grid">
+          <div className="grid-image">
+            <div className="grid-one">
+              <div>
+                <Container>
+                  <h5>| REPEAT WORD IDENTIFIER |</h5>
+                  <p style={{ textAlign: "justify", padding: "5px" }}>
+                    👋 Here, I have attached my Repeated Words Identifier
+                    project. 🚀 This project was created using HTML, CSS,
+                    Bootstrap, and JavaScript. 📋 You can paste any paragraph
+                    into our website to find repeated words. 🔍
+                  </p>
+                  <div
+                    style={{ display: "flex", justifyContent: "space-evenly" }}
+                  >
+                    <Button variant="outline-dark">DEMO</Button>
+                    <Button variant="outline-dark">CODE</Button>
+                  </div>
+                </Container>
+              </div>
+            </div>
+            <img src={rwi3} />{" "}
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
+          </div>
+          <div className="grid-image">
+            {" "}
+            <img src={rwi1} />
           </div>
         </div>
       </section>
